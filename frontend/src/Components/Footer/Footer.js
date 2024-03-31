120bcadb5379181f573bff6bd1299d67005962e3
trplx666 <russiansoveet@gmail.com>
2024-03-31 13:34:29 +0300

    add correct links

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -9,12 +9,12 @@ const Footer = () => (
     <div className="footer-container">
       <Container fluid style={{ backgroundColor: '#333', color: '#fff'}}>           
         <Container style= {{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '10px'}}>
-          <p style={{ marginBottom: '10px' }}>© Cloting Shop</p>
+          <p style={{ marginBottom: '10px' }}>© Clothing Shop</p>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '-10px' }}> 
-            <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://telegram.org/" target="_blank" rel="noopener noreferrer">
               <img src={telegram} alt="telegram" style={{ width: '40px', marginRight: '15px' }} />
             </a>
-            <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://vk.com" target="_blank" rel="noopener noreferrer">
               <img src={vk} alt="vk" style={{ width: '40px' }} />
             </a>
           </div>
