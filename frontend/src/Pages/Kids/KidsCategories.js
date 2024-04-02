91908f8529c81f21c147c0d7ea70e57574cffca6
trplx666 <russiansoveet@gmail.com>
2024-04-02 13:53:01 +0300

    kids update

--- frontend/src/Pages/Kids/KidsCategories.js
+++ frontend/src/Pages/Kids/KidsCategories.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Babies from './1.jpg';
-import Little from './2.jpg';
-import Big from './3.jpg';
-import Oversize from './4.jpg';
+import Accessories from './kids acs.webp';
+import Shoes from './kids shoes.png';
+import Clothes from './kids clothes.jpg';
 import './KidsCategories.css';
 
-const KidsCategories = ({ imageWidth, imageHeight }) => {
+const KidsCategories = () => {
   return (
-    <div className="kids-block" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div className="kids-row" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
-        <Link to="/kids/babies" className="kids-image-container" style={{ margin: '5px' }}>
-          <img src={Babies} alt="Babies" style={{ width: "300px", height: '400px' }} />
-          <p className="image-alt">Babies and Toddlers</p>
-        </Link>
-        <Link to="/kids/Little-kids" className="kids-image-container" style={{ margin: '5px' }}>
-          <img src={Little} alt="Little-kids" style={{ width: '300px', height: '400px' }} />
-          <p className="image-alt">Little Kids</p>
+    <div className="kids-block">
+      <div className="kids-image-container">
+        <Link to="/kids/clothes">
+          <img src={Clothes} alt="Clothes" />
+          <p className="image-alt">Clothes</p>
         </Link>
       </div>
-      <div className="kids-row" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
-        <Link to="/kids/big-kids" className="kids-image-container" style={{ margin: '5px' }}>
-          <img src={Big} alt="Big-kids" style={{ width: "300px", height: "400px" }} />
-          <p className="image-alt">Big Kids</p>
+      <div className="kids-image-container">
+        <Link to="/kids/shoes">
+          <img src={Shoes} alt="Shoes" />
+          <p className="image-alt">Shoes</p>
         </Link>
-        <Link to="/kids/oversized-fits" className="kids-image-container" style={{ margin: '5px' }}>
-          <img src={Oversize} alt="Oversized-fits" style={{ width: "300px", height: "400px" }} />
-          <p className="image-alt">Oversized fits</p>
+      </div>
+      <div className="kids-image-container">
+        <Link to="/kids/accessories">
+          <img src={Accessories} alt="Accessories" />
+          <p className="image-alt">Accessories</p>
         </Link>
       </div>
     </div>
