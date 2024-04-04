7fa9a227df78bd3e083d36d6df1925d6386046f7
trplx666 <russiansoveet@gmail.com>
2024-04-05 01:23:43 +0300

    product-info page

--- frontend/src/Pages/Product-info/Details.js
+++ frontend/src/Pages/Product-info/Details.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { useProductInfo } from './UseProductInfo';
+import { Card, Row, Col, CardImg, CardBody, CardTitle, CardText, CardSubtitle, Button } from 'react-bootstrap';
+import './Details.css'; // Импорт файла стилей
 
-const Details = () => {
+const ProductInfo = () => {
+  const { product, getImage, selectedSize, selectedColor, setSelectedSize, setSelectedColor } = useProductInfo();
+
+  if (!product || !product.attributes) {
+    return null;
+  }
+
+  const { attributes } = product;
+
   return (
-    <div>Details</div>
-  )
-}
+    <Card className="product-details"> {/* Добавляем класс product-details */}
+      <Row>
+        <Col sm="12" md="4">
+          <div className="sizes"> {/* Добавляем класс sizes */}
+            <CardImg
+              left="true"
+              width="100%"
+              src={`http://localhost:1337${getImage(selectedColor)}`}
+              alt=""
+            />
+          </div>
+        </Col>
+        <Col sm="12" md="8">
+          <CardBody>
+            <CardTitle>{attributes.name}</CardTitle>
+            <CardText>{attributes.description}</CardText>
+            <CardSubtitle>
+              <strong>Price: £{attributes.price}</strong>
+            </CardSubtitle>
+            <div className="sizes"> {/* Добавляем класс sizes */}
+              {attributes.sizes.map((size) => (
+                <span
+                  key={size.name}
+                  className={`${selectedSize === size.name ? "active" : ""}`}
+                  onClick={() => setSelectedSize(size.name)}
+                >
+                  {size.name}
+                </span>
+              ))}
+            </div>
+            <div className="colours"> {/* Добавляем класс colours */}
+              {attributes.colours.map((colour) => (
+                <span
+                  key={colour.name}
+                  className={`${selectedColor === colour.name ? "active" : ""}`}
+                  onClick={() => setSelectedColor(colour.name)}
+                >
+                  <img
+                    src={`http://localhost:1337${getImage(colour.name)}`}
+                    alt={colour.name}
+                  />
+                </span>
+              ))}
+            </div>
+            <Button color="primary">Add to basket</Button>
+          </CardBody>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
 
-export default Details
+export default ProductInfo;
--- frontend/src/Pages/Product-info/useProductInfo.js
+++ frontend/src/Pages/Product-info/useProductInfo.js
@@ -1,8 +1,57 @@
 import axios from "axios";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-
 export const useProductInfo = () => {
-    
-}
+    const { id: productId } = useParams();
+    const [product, setProduct] = useState({});
+    const [selectedColor, setSelectedColor] = useState("");
+    const [selectedSize, setSelectedSize] = useState("");
+
+
+
+
+    const getImage = (colour) => {
+        const { attributes } = product;
+        const image = attributes.images.data.find((image) =>
+            image.attributes.name.includes(colour)
+        );
+
+
+        return image.attributes.url || "";
+    };
+
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const { data: { data } } = await axios.get(`http://localhost:1337/api/products/${productId}?populate=*`);
+                setProduct(data);
+            } catch (error) {
+                console.log({ error });
+            }
+        };
+
+        if (productId) {
+            fetchCategories();
+        }
+    }, [productId]);
+
+    useEffect(() => {
+        if (product && product.attributes) {
+            const { attributes } = product;
+            setSelectedColor(attributes.colours[0].name);
+            setSelectedSize(attributes.sizes[0].name);
+        }
+    }, [product, setSelectedColor, setSelectedSize]);
+
+
+    return {
+        product,
+        getImage,
+        selectedSize,
+        selectedColor,
+        setSelectedSize,
+        setSelectedColor,
+    };
+};
