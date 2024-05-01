16d10e0c238c8964ea52510c243f79c2c756e969
trplx666 <russiansoveet@gmail.com>
2024-05-01 13:31:43 +0300

    logout modal

--- frontend/src/Pages/Profile/Profile.js
+++ frontend/src/Pages/Profile/Profile.js
@@ -1,20 +1,29 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { IoPersonCircleOutline } from "react-icons/io5";
+import axios from "axios";
 import './Profile.css';
 import { userData } from "../../helpers";
 
 const Profile = ({ token }) => {
   const [user, setUser] = useState({});
   const [isUserUpdated, setisUserUpdated] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem('user');
-    navigate('/login');
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+
+  const handleLogout = async () => {
+    try {
+      localStorage.removeItem('user');
+      navigate('/login');
+    } catch (error) {
+      console.log({ error });
+    }
   };
 
   useEffect(() => {
@@ -45,11 +54,23 @@ const Profile = ({ token }) => {
         <p>Name: {user.username}</p>
         <p>Email: {user.email}</p>
         <p>Account created at: {new Date(user.createdAt).toLocaleDateString()}</p>
-        <Button variant="primary">Change password</Button>{' '} <Button variant="primary">Delivery information</Button>{' '}
-        <p style = {{ marginTop: '10px'}} >
-        <Button variant="danger" onClick={handleLogout} >Log out</Button>{' '}
+        <Button variant="primary">Change password</Button>{' '}
+        <Button variant="primary">Delivery information</Button>{' '}
+        <p style={{ marginTop: '10px' }}>
+          <Button variant="danger" onClick={handleShowModal}>Log out</Button>{' '}
         </p>
       </div>
+
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to log out?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>No</Button>
+          <Button variant="danger" onClick={handleLogout}>Yes</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
