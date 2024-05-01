cda109f84631d685d87d8f600e3d33db10c63c32
trplx666 <russiansoveet@gmail.com>
2024-05-01 12:34:40 +0300

    fix

--- frontend/src/Components/Chat/chat.js
+++ frontend/src/Components/Chat/chat.js
@@ -1,27 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaComment } from 'react-icons/fa';
-import ChatApp from './ChatApp'; // Импортируем компонент ChatApp
 import './chat.css';
 
-const ChatIcon = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleChat = () => {
-    setIsOpen(!isOpen);
-  };
-
+function ChatIcon() {
   return (
     <div className="chat-container">
-      <div className="chat-icon" onClick={toggleChat}>
+      <div className="chat-icon" >
         <FaComment />
       </div>
-      {isOpen && (
-        <div className="chat-window">
-          <ChatApp /> {/* Отображаем ChatApp при открытом окне чата */}
-        </div>
-      )}
     </div>
   );
-};
+}
 
 export default ChatIcon;
